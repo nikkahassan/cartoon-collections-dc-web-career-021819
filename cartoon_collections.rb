19ada19a9350b342fc0ce09f3a44da204e13f0ea
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:36:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,10 +1,12 @@
 def roll_call_dwarves(array)
   i = 0 
   while i < array.length 
-  puts "#{i + 1}. #{array[i]}"
+    puts "#{i + 1}. #{array[i]}"
     i += 1 
   end 
 end
+
+roll_call_dwarves(dwarves)
 
 def summon_captain_planet(array)
   new_array = []
@@ -14,16 +16,27 @@ def summon_captain_planet(array)
     i += 1 
   end 
   new_array
+end
 
-def long_planeteer_calls)(array)
+summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(array) 
   i = 0 
   if  array.any? {|i| i.length > 4}
     return true
   else 
     return false
   i = i + 1
-  end
+  end 
 end
+
+long_planeteer_calls(call_screams)
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
@@ -31,3 +44,7 @@ def find_the_cheese(array)
     cheese_types.include?(type)
   end 
 end
+
+find_the_cheese(snacks)
+
+
