803a130c77d6351016f04c01feb8c8d503c529b8
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-06 17:33:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,14 @@ def summon_captain_planet(array)
   end 
   new_array
 
-def long_planeteer_calls
+def long_planeteer_calls)(array)
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end
 end
 
 def find_the_cheese# code an argument here
